87915d8c8c5c424fc6070d964fb92d8a832f84e6
Jeff Kungu Gitenguri <jgitenguri@safaricom.co.ke>
2020-02-19 12:05:30 +0300

    [feature] adds data binding

--- app/src/main/java/com/jeff/moviedb/view/MainActivity.java
+++ app/src/main/java/com/jeff/moviedb/view/MainActivity.java
@@ -2,6 +2,7 @@ package com.jeff.moviedb.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,6 +15,7 @@ import android.os.Bundle;
 
 import com.jeff.moviedb.R;
 import com.jeff.moviedb.adapter.MovieAdapter;
+import com.jeff.moviedb.databinding.ActivityMainBinding;
 import com.jeff.moviedb.model.Movie;
 import com.jeff.moviedb.model.MovieDBResponse;
 import com.jeff.moviedb.service.MovieDataService;
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Movie> movies;
     private RecyclerView recyclerView;
     private MovieAdapter movieAdapter;
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Jeff Kungu");
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         getPopularMovies();
     }
@@ -68,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void showOnRecyclerView() {
 
-        recyclerView = (RecyclerView) findViewById(R.id.rvMovies);
+        recyclerView = activityMainBinding.rvMovies;
         movieAdapter = new MovieAdapter(this, movies);
 
         if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
